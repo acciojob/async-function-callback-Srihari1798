757700bd89412b2affe042682b302b82afbb7ba2
Srihari1798 <120098197+Srihari1798@users.noreply.github.com>
2023-03-19 23:37:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
 
 //your JS code here. If required.
+const output = document.getElementById('output');
+      const fetchData = async (callback) => {
+        try {
+          const response = await fetch(url);
+          const data = await response.json();
+          callback(data.title);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      const displayMessage = (message) => {
+        output.innerHTML = message;
+      };
+      document.getElementById('btn').addEventListener('click', () => {
+        fetchData(displayMessage);
+      });
